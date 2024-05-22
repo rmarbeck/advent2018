a2917c4445f98db5994d149aa61ab3b0a090a44d
rmarbeck <rafoufou>
2024-05-22 16:00:38 +0200

    Day 3 OK

--- day3/src/main/scala/Solution.scala
+++ day3/src/main/scala/Solution.scala
@@ -1,23 +1,22 @@
+import scala.collection.immutable.BitSet
+import scala.collection.mutable
+
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
     val claims = inputLines.collect:
-      case s"#$id @ $left,$top: ${width}x$height" => Claim(id, (left.toInt, top.toInt), (width.toInt, height.toInt))
+      case s"#$id @ $left,$top: ${width}x$height" =>
+        val List(ci, cl, ct, cw, ch) = List(id, left, top, width, height).map(_.toInt)
+        Claim(ci, (cl, ct), (cw, ch))
 
+    val square = Square()
+    claims.foreach(square.add)
 
+    val resultPart1 = square.multipleClaims
+    val resultPart2 = square.singleClaim
 
-    //
-    // Code is here
-    //
-
-    /**
-     * #1 @ 1,3: 4x4
-     * #2 @ 3,1: 4x4
-     * #3 @ 5,5: 2x2
-     */
-
-    val result1 = s""
-    val result2 = s""
+    val result1 = s"$resultPart1"
+    val result2 = s"$resultPart2"
 
     (s"${result1}", s"${result2}")
 
@@ -26,4 +25,31 @@ end Solution
 type LeftTop = (Int, Int)
 type SquareSize = (Int, Int)
 
-case class Claim(id: String, leftTop: LeftTop, size: SquareSize)
+type Coords = (Int, Int)
+
+class Square:
+  import scala.collection.mutable.Map
+  def multipleClaims: Int = content.count(_._2.size > 1)
+  def singleClaim: String =
+    val claimsIds = content.values.fold(BitSet.empty)(_ ++ _)
+    content.values.foldLeft(claimsIds):
+      case (acc, currentValue) if currentValue.size > 1 => acc -- currentValue
+      case (acc, _ ) => acc
+    .mkString(",")
+
+  private val content: mutable.Map[Coords, Set[Int]] = mutable.Map()
+
+  private def add(coords: Coords, claimId: Int): Square =
+    content.update(coords, content.getOrElse(coords, Set.empty) + claimId)
+    this
+
+  def add(claim: Claim): Unit =
+    for
+      row <- claim.rows
+      col <- claim.cols
+    do
+      add((row, col), claim.id)
+
+case class Claim(id: Int, leftTop: LeftTop, size: SquareSize):
+  lazy val rows: Seq[Int] = Iterator.from(leftTop._2).take(size._2).toSeq
+  lazy val cols: Seq[Int] = Iterator.from(leftTop._1).take(size._1).toSeq
--- day3/src/test/scala/MySuite.scala
+++ day3/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day3 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "105071")
+    assertEquals(score2, "222")
 
   test("Day3 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "4")
+    assertEquals(score2, "3")
