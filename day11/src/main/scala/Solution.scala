ac8cb82bd4446688211b14d294df683c2f6ca42a
rmarbeck <rafoufou>
2024-12-14 12:46:45 +0100

    Day 11 OK with SummedAreaTable

--- day11/src/main/scala/Solution.scala
+++ day11/src/main/scala/Solution.scala
@@ -1,121 +1,65 @@
 import scala.collection.mutable
 import scala.collection.parallel._
 
-type Grid = Map[(Int, Int), Int]
 type GridSerial = Int
+type SummedAreaTable = Map[(Int, Int), Int]
+val gridSize: Int = 300
 
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
     given GridSerial = inputLines.head.toInt
 
-    given scores: Grid =
-      (for
-        x <- 1 to 300; y <- 1 to 300
-      yield
-        (x, y) -> score(x, y)
-        ).toMap
+    given SummedAreaTable = initializeSummedAreaTable
 
-    val result = findBest(3, 3)
+    val resultPart1 = findFromSummedAreaTable(3, 3)
 
-    val resultPart2 = findBest(1, 300)
+    val resultPart2 = findFromSummedAreaTable(1, gridSize)
 
-    val result1 = s"${result._1},${result._2}"
-    val result2 = s"${resultPart2._1},${resultPart2._2},${resultPart2._3}"
+    val result1 = s"${resultPart1.take(2).mkString(",")}"
+    val result2 = s"${resultPart2.mkString(",")}"
 
     (s"$result1", s"$result2")
 
-def findBest(sizeMin: Int, sizeMax: Int)(using grid: Grid): (Int, Int, Int) =
-  def findBest(size: Int): ((Int, Int), Int) =
-    val (deltaMin, deltaMax): (Int, Int) = size match
-      case value if value % 2 == 0 => ((size / 2) - 1, size / 2)
-      case value => (size / 2, size / 2)
-    (for
-      x <- 1 + deltaMin to 300 - deltaMax; y <- 1 + deltaMin to 300 - deltaMax
-    yield
-      val (topLeft, score) = Cache.scoreCached(x, y, size)
-      topLeft -> score
-    ).maxBy(_._2)
+def initializeSummedAreaTable(using gridSerial: GridSerial): SummedAreaTable =
+  val data: mutable.Map[(Int, Int), Int] = mutable.Map().withDefaultValue(0)
 
-  val (((bestX, bestY), value), size) =
-    Range(sizeMin, sizeMax + 1).map:
-      size =>
-        print(".")
-        (findBest(size), size)
-    .maxBy(_._2)
+  val indices = 1 to gridSize
+  for
+    x <- indices; y <- indices
+  do
+    data.update((x, y), score(x, y) + data(x - 1, y) + data(x, y - 1) - data(x - 1, y - 1))
 
-  (bestX, bestY, size)
+  data.toMap.withDefaultValue(0)
 
+def findFromSummedAreaTable(sizeMin: Int, sizeMax: Int)(using summedArea: SummedAreaTable): List[Int] =
+  def findBest(currentSize: Int): (Int, Int, Int, Int) =
+    val (bestX, bestY) -> bestScore =
+      val indices = 1 to gridSize - currentSize + 1
+      (for
+        x <- indices; y <- indices
+        List(xMax, yMax) = List(x, y).map(_ + currentSize - 1)
+      yield
+        val topLeftOutside        = (x - 1, y - 1)
+        val bottomLeftOutside     = (x - 1,  yMax)
+        val bottomLRightIncluded  = (xMax ,  yMax)
+        val topRightOutside       = (xMax , y - 1)
 
-object Cache:
-  val cacheForSquareList: mutable.Map[Int, List[(Int, Int)]] = mutable.Map()
-  val cacheForScores: mutable.Map[(Int, Int, Int), ((Int, Int), Int)] = mutable.Map()
+        (x, y) -> (summedArea(bottomLRightIncluded) + summedArea(topLeftOutside) - summedArea(bottomLeftOutside) - summedArea(topRightOutside))
+      ).maxBy(_._2)
 
-  def scoreCached(x: Int, y: Int, size: Int)(using grid: Grid): ((Int, Int), Int) =
-    cacheForScores.getOrElseUpdate((x, y, size), around(x, y)(size))
+    (bestX, bestY, bestScore, currentSize)
 
-  def squareCached(size: Int): List[(Int, Int)] =
-    cacheForSquareList.getOrElseUpdate(size, square(size))
+  import collection.parallel.CollectionConverters.RangeIsParallelizable
+  val (bestX, bestY, _ , size) = (sizeMin to sizeMax).par.map(findBest).maxBy(_._3)
 
-def square(size: Int): List[(Int, Int)] =
-  val (deltaMin, deltaMax): (Int, Int) = size match
-    case value if value % 2 == 0 => ((size / 2) - 1, size / 2)
-    case value => (size / 2, size / 2)
-
-  (for x <- -deltaMin to deltaMax; y <- -deltaMin to deltaMax
-  yield (x, y)
-   ).toList
-
-def square(pseudoCenter: (Int, Int), size: Int): List[(Int, Int)] =
-  val (deltaMin, deltaMax): (Int, Int) = size match
-    case value if value % 2 == 0 => ((size / 2) - 1, size / 2)
-    case value => (size / 2, size / 2)
-
-  (for
-    x <- (pseudoCenter._1 - deltaMin) to (pseudoCenter._1 + deltaMax)
-    y <- (pseudoCenter._2 - deltaMin) to (pseudoCenter._2 + deltaMax)
-  yield
-    (x, y)
-    ).toList
-
-def around(x: Int, y: Int)(size: Int)(using grid: Grid): ((Int, Int), Int) =
-  size match
-    case 1 => ((x, y) ,grid(x,y))
-    case _ =>
-      size % 2 match
-        case 0 =>
-          val row =
-            for
-              valX <- (x - size / 2) + 1 to x + size / 2
-            yield
-              grid(valX, y + size / 2)
-          val col =
-            for
-              valY <- (y - size / 2) + 1 to y + size / 2
-            yield
-              grid(x + size / 2, valY)
-
-          (((x - size / 2) + 1, (y - size / 2) + 1),  Cache.scoreCached(x, y, size - 1)._2 + row.sum + col.sum)
-
-        case _ =>
-          val row =
-            for
-              valX <- (x - size / 2) to x + size / 2
-            yield
-              grid(valX, y - size / 2)
-          val col =
-            for
-              valY <- (y - size / 2) to y + size / 2
-            yield
-              grid(x - size / 2, valY)
-
-          ((x - size / 2, y - size / 2), Cache.scoreCached(x, y, size - 1)._2 + row.sum + col.sum)
+  List(bestX, bestY, size)
 
 
 def score(x: Int, y: Int)(using gridSerial: GridSerial): Int =
   val rackId = x + 10
   val base = ((rackId * y) + gridSerial) * rackId match
     case value if value >= 100 => value.toString.dropRight(2).last.asDigit
-    case  _=> 0
+    case _ => 0
 
   base - 5
